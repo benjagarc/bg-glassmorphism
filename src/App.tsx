0178727de2873bb10a5d9ee27de108adbd9832b0
bgarciabtk <bgarcia@btkfarma.com>
2025-02-06 16:13:54 -0600

    set squares to test classes

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,13 @@
 // import './styles/globals.scss';
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.scss'
-import Button from './components/Button';
+// import reactLogo from './assets/react.svg'
+// import viteLogo from '/vite.svg'
+import "./App.scss";
+// import Button from "./components/Button";
 
 function App() {
-
   return (
     <>
-      <div>
+      {/* <div>
         <a href="https://vite.dev" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />
         </a>
@@ -16,13 +15,57 @@ function App() {
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
       </div>
-      <h1>Vite + React</h1>
-      <Button></Button>
-      <p className="read-the-docs">
+      <h1>Vite + React</h1> */}
+      <div
+        style={{
+          display: "flex",
+          gap: "1rem",
+          width: "100vw",
+          justifyContent: "center",
+          alignItems: "center",
+          marginBottom: "1rem"
+        }}
+      >
+        <div
+          style={{ display: "block", width: "200px", height: "200px" }}
+          className="bg-dark"
+        ></div>
+        <div
+          style={{ display: "block", width: "200px", height: "200px" }}
+          className="bg-x2dark"
+        ></div>
+        <div
+          style={{ display: "block", width: "200px", height: "200px" }}
+          className="bg-x3dark"
+        ></div>
+      </div>
+      <div
+        style={{
+          display: "flex",
+          gap: "1rem",
+          width: "100vw",
+          justifyContent: "center",
+          alignItems: "center",
+        }}
+      >
+        <div
+          style={{ display: "block", width: "200px", height: "200px" }}
+          className="bg-light"
+        ></div>
+        <div
+          style={{ display: "block", width: "200px", height: "200px" }}
+          className="bg-x2light"
+        ></div>
+        <div
+          style={{ display: "block", width: "200px", height: "200px" }}
+          className="bg-x3light"
+        ></div>
+      </div>
+      {/* <p className="read-the-docs">
         Click on the Vite and React logos to learn more
-      </p>
+      </p> */}
     </>
-  )
+  );
 }
 
-export default App
+export default App;
