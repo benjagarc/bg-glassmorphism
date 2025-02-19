94120aefa85ed0a3ccb655cbe77ea6c29ca990ed
bgarciabtk <bgarcia@btkfarma.com>
2025-02-19 13:17:08 -0600

    add sass to config

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -1,4 +1,5 @@
 import type { StorybookConfig } from "@storybook/react-vite";
+import path from "path";
 
 const config: StorybookConfig = {
   stories: ["../src/**/*.mdx", "../src/**/*.stories.@(js|jsx|mjs|ts|tsx)"],
@@ -11,6 +12,30 @@ const config: StorybookConfig = {
   framework: {
     name: "@storybook/react-vite",
     options: {},
+  },
+  viteFinal: async (config) => {
+    if (!config.css) config.css = {};
+    if (!config.resolve) config.resolve = {};
+    config.resolve= {
+      alias: {
+        "@components": path.resolve(__dirname, "../src/components"),
+        "@styles": path.resolve(__dirname, "../src/styles"),
+      },
+    };
+    config.css.preprocessorOptions = {
+      scss: {
+        additionalData: `
+          @use "@styles/_colors.scss" as *;
+          @use "@styles/_fonts.scss" as *;
+          @use "@styles/_media_queries.scss" as *;
+          @use "@styles/_spaces_borders.scss" as *;
+          @use "@styles/_transitions_animations.scss" as *;
+          @use "@styles/_reset_css.scss" as *;
+          @use "@styles/globals.scss" as *;
+        `,
+      },
+    };
+    return config;
   },
 };
 export default config;
